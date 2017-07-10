744093a7903dd6f53b0e4db204387412e5d77eba
mixassio <mixail.bondarev@gmail.com>
2017-07-10 23:41:46 +0300

    send for first check

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+The script searches for duplicate files by the specified path.
+        The input accepts the directory, if it is not specified, by default - the root directory.
+        A duplicate is a file with the same size and name.
+
+# How use
+
+Example of script launch on Linux, Python 3.5:
+
+usage: duplicates.py [-filepath] filepath
+
+if filepath is None, filepath = '/'
+
+```#!bash
+
+$ python3 duplicates.py -f /Users/mixassio/test
+file = "java_error_in_pycharm_621.log", size = "82734"
+['/Users/mixassio/test/java_error_in_pycharm_621.log', '/Users/mixassio/test/test2/java_error_in_pycharm_621.log'
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,42 @@
+import os
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filepath', nargs='?', type=str)
+    return parser
+
+
+def create_filelist(filepath):
+    tree = os.walk(filepath)
+    files = {}
+    for element in tree:
+        for file in element[2]:
+            dir_file = element[0] + '/' + file
+            if os.path.isfile(dir_file):
+                size_file = os.path.getsize(dir_file)
+                try:
+                    files[size_file][file].append(dir_file)
+                except KeyError:
+                    files[size_file] = {file: [dir_file, ]}
+    return files
+
+
+def print_result(files):
+    for size, file in files.items():
+        for file, paths in file.items():
+            if len(paths) > 1:
+                print('file = "{}", size = "{}"'.format(file, size))
+                print(paths)
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    namespace = parser.parse_args()
+    if namespace.filepath:
+        filepath_in = os.path.abspath(namespace.filepath)
+    else:
+        filepath_in = os.path.abspath('/')
+
+    files = create_filelist(filepath_in)
+    print_result(files)
